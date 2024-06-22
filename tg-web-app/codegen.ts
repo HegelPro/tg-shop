404115c4e2f765c60d16bc9ca1b1db4269313f66
HegelPro <palkin.leo@gmail.com>
2024-06-22 22:23:12 +0400

    chore: move graphql endpoint to .env

--- tg-web-app/codegen.ts
+++ tg-web-app/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://tg-store.site",
+  schema: process.env.VITE_GRAPHQL_ENDPOINT,
   documents: "src/**/*.tsx",
   generates: {
     "src/gql/": {
--- tg-web-app/src/util/graphQLClient.ts
+++ tg-web-app/src/util/graphQLClient.ts
@@ -1,3 +1,3 @@
 import { GraphQLClient } from "graphql-request";
 
-export const graphQLClient = new GraphQLClient('https://tg-store.site')
+export const graphQLClient = new GraphQLClient(import.meta.env.VITE_GRAPHQL_ENDPOINT)
