214f711b5df83d6f2a789cdc4313c2d001f33036
HegelPro <palkin.leo@gmail.com>
2024-06-23 17:34:14 +0400

    feat: replace frontend prodaction app to server #4

--- graphql/src/server.ts
+++ graphql/src/server.ts
@@ -1,15 +1,18 @@
 import { createYoga } from 'graphql-yoga'
-import { createServer } from 'https'
 import { schema } from './schema'
 import { bot } from "./bot/bot"
-import { addCommands } from './bot/addCommands'
 import { readFileSync } from 'fs'
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import path from 'path'
+import fastifyStatic from '@fastify/static'
 
-addCommands(bot)
 bot.start({onStart: () => console.log('bot is started')});
 
-const yoga = createYoga({
-  graphqlEndpoint: '/',
+const yoga = createYoga<{
+  req: FastifyRequest
+  reply: FastifyReply
+}>({
+  graphqlEndpoint: process.env.GRAPHQL_ENDPOINT,
   schema,
   context: (req) => {
     return {
@@ -23,12 +26,41 @@ const key = readFileSync(process.env?.CERTIFICATE_KEY_PATH || '', 'utf8');
 const cert = readFileSync(process.env?.CERTIFICATE_CERT_PATH || '', 'utf8');
 const ca = readFileSync(process.env?.CERTIFICATE_CHAIN_PATH || '', 'utf8');
 
-const server = createServer({key, cert, ca}, yoga)
-const serverPort = process.env?.SERVER_PORT || 4000
+const app = fastify({ logger: true, https: {
+  key,
+  cert,
+  ca
+},
+})
 
-server.listen(serverPort, () => {
-  console.log(`\
-🚀 Server ready at: http://127.0.0.1:${serverPort}
-⭐️ See sample queries: http://pris.ly/e/ts/graphql#using-the-graphql-api
-  `)
+const serverPort = Number.parseInt(process.env?.SERVER_PORT || '4000')
+
+const frontendPath = path.join(__dirname, '../../tg-web-app/dist')
+app.register(fastifyStatic, {
+  root: frontendPath,
+})
+
+app.route({
+  url: yoga.graphqlEndpoint,
+  method: ['GET', 'POST', 'OPTIONS'],
+  handler: async (req, reply) => {
+    const response = await yoga.handleNodeRequestAndResponse(req, reply, {
+      req,
+      reply
+    })
+    response.headers.forEach((value, key) => {
+      reply.header(key, value)
+    })
+ 
+    reply.status(response.status)
+ 
+    reply.send(response.body)
+ 
+    return reply
+  }
+})
+ 
+app.listen({
+  port: serverPort,
+  host: '0.0.0.0'
 })
